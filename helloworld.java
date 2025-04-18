bd2b76a139ae75119222a5acfdc4f2818c0a764f
Caio Vinicius <140816471+Isuneblack29@users.noreply.github.com>
2025-04-18 14:13:50 -0300

    Add files via upload

---
+++ helloworld.java
@@ -0,0 +1,6 @@
+public class helloworld {
+    public static void main(String[] args) throws Exception {
+        System.out.println("MEU PRIMEIRO PROJETO NO JAVA");
+        System.out.println("OLÁ MUNDO!!");
+    }
+}
